391c852ba74980fea37fa26a59af494ae9a5818a
Juan Bachiller <jjbachiller@gmail.com>
2017-12-01 20:15:03 +0100

    Corrections to pass list id in the url + new test to check items are added to an existing list

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,9 @@ def home_page(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST.get('item_text'), list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
 
 def view_list(request, list_id):
-    items = Item.objects.all()
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
