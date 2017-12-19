bcf948999151344c44de538c45700d3531fa78fc
Juan <jjbachiller@gmail.com>
2017-12-19 11:53:11 +0100

    New tests + code for the authentication de-spiking logic

---
+++ accounts/authentication.py
@@ -0,0 +1,7 @@
+from accounts.models import User, Token
+
+class PasswordlessAuthenticationBackend(object):
+
+    def authenticate(self, uid):
+        token = Token.objects.get(uid=uid)
+        return User.objects.get(email=token.email)
